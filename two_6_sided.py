b3c8260d2b6d0e0f72f48b9add07f9488c58c17d
sadiii40 <schalise@student.cscc.edu>
2026-04-27 16:52:40 -0400

    Week prompt: dice rolling histogram assignment

---
+++ two_6_sided.py
@@ -0,0 +1,32 @@
+#Question 2:
+#Create different histogram charts for rolling two six-sided die, or two eight-sided die. Use the example from the textbook resources, “dice_visual.py.” Discuss the     differences and similarities of the distribution of the results. In general, how does this correlate with the distribution of two n-sided die. Search for relevant discussions on the internet.
+#Two 6 sided dice
+
+import plotly.express as px
+from die import Die
+
+#create two D6 dice
+die_1 = Die()
+die_2 = Die()
+
+#roll dice
+results = []
+for roll in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+#analyze
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+
+frequencies = []
+for value in poss_results:
+    frequencies.append(results.count(value))
+
+#plot
+title = "Results of rolling two D6 Dice "
+labels = {'x' : 'Result' , 'y' : 'Frequency' }
+
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.update_layout(xaxis_dtick=1)
+fig.show()
